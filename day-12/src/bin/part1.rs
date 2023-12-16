aa4ce9300e33bd8a1b0b31844e59636392670c42
Kantor Hunor-Akos <kantorakos99@gmail.com>
2023-12-16 22:22:48 +0200

    twelfth day: part1 recursion, I start hating the borrow checker

--- day-12/src/bin/part1.rs
+++ day-12/src/bin/part1.rs
@@ -1,29 +1,56 @@
-use std::fs::read_to_string;
+use std::{fs::read_to_string, ops::Index};
 
-fn is_valid(springs: &str, numbers: Vec<u8>) -> bool {
-    let springs = springs.split(".").filter(|s| !s.is_empty() );
-    let mut numbers = numbers.clone();
-    numbers.reverse();
-    for spring in springs {
-        if let Some(number) = numbers.pop() {
-            println!("{}:{}", number, spring);
-            if number != spring.len() as u8 {
-                return false;
-            }
+fn count_dot(springs: &[u8], numbers: &mut Vec<u8>) -> u128 {
+    count(&springs[1..], numbers)
+}
+
+fn count_hash(springs: &[u8], numbers: &mut Vec<u8>) -> u128 {
+    let number = numbers[0] as usize;
+    if springs.len() < number {
+        return 0;
+    }
+    if springs[..number].contains(&b'.') {
+        return 0;
+    }
+    if springs.len() == number {
+        if numbers.len() == 1 {
+            return 1;
         } else {
-            return false;
+            return 0;
         }
     }
-    true
+    if springs[number] == b'?' || springs[number] == b'.' {
+        return count(&springs[number+1..], &mut numbers[1..].to_vec());
+    }
+    0
+}
+
+fn count(springs: &[u8], numbers: &mut Vec<u8>) -> u128 {
+    if numbers.is_empty() {
+        return !springs.contains(&b'#') as u128;
+    }
+
+    if springs.is_empty() {
+        return 0;
+    }
+
+    let spring = springs.index(0);
+
+    match spring {
+        b'.' => count_dot(springs, numbers),
+        b'#' => count_hash(springs, numbers),
+        b'?' => count_dot(springs, numbers) + count_hash(springs, numbers),
+        _ => panic!("Unknown character in springs")
+    }
 }
 
 fn main() {
-    let mut sum = 0;
-    for line in read_to_string("../test_data.txt").unwrap().lines() {
+    let mut sum: u128 = 0;
+    for line in read_to_string("../data.txt").unwrap().lines() {
         let (springs, numbers) = line.split_once(" ").unwrap();
-        let numbers: Vec<u8> = numbers.split(",").map(|n| n.parse::<u8>().unwrap() ).collect();
-
-        println!("{}", is_valid(springs, numbers));
+        let springs = springs.as_bytes();
+        let mut numbers: Vec<u8> = numbers.split(",").map(|n| n.parse::<u8>().unwrap() ).collect();
+        sum += count(springs, &mut numbers);
     }
-    println!("{}", sum);
+    println!("Total sum: {}", sum);
 }
